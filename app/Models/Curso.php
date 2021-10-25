a71ee70f12e7311c02546c6c8313836a97c4358d
Herbert Rojas <rey_herbert18@outlook.com>
2021-10-25 11:54:09 -0500

    Hito 1 - Modelos, tabla Usuarios,Cursos, Categorias

--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -4,8 +4,61 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Builder;
 
 class Curso extends Model
 {
     use HasFactory;
+
+    protected $fillable=[
+        'id','titulo','descripcion','user_id','imagen',
+        'precio','destacado','estado_curso_id'
+
+    ];
+    /**
+     * The categorias that belong to the Curso
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function categorias(): BelongsToMany
+    {
+        return $this->belongsToMany(Categoria::class)->withTimestamps();
+    }
+
+    /**
+     * Get the docente that owns the Curso
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function docente(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    /**
+     * The estudiantes that belong to the Curso
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function estudiantes(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'curso_estudiante', 'user_id', 'curso_id')->withTimestamps();
+    }
+
+    public function scopeFiltered(Builder $builder, Categoria $categoria = null){
+        $builder->with("docente", "categorias", "wishlists");
+        $builder->withCount("estudiantes");
+        $builder->where("status", 'Publicado');
+        if (session()->has('search[cursos]')) {
+            $builder->where('titulo', 'LIKE', '%' . session('search[cursos]') . '%');
+        }
+
+        if ($categoria){
+            $builder->whereHas("categorias", function (Builder $table) use ($categoria) {
+               $table->where("id", $categoria->id);
+            });
+        }
+        return $builder->paginate();
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Storage::deleteDirectory('categorias');
+        Storage::makeDirectory('categorias');
+        Storage::deleteDirectory('cursos');
+        Storage::makeDirectory('cursos');
+        Storage::deleteDirectory('unidades');
+        Storage::makeDirectory('unidades');
         // \App\Models\User::factory(10)->create();
+        $this->call([
+            RoleSeeder::class,
+            UserSeeder::class,
+            EstadoCursoSeeder::class,
+            CategoriaSeeder::class,
+            UnidadTipoSeeder::class,
+            CursoSeeder::class,
+        ]);
     }
 }
